cd5b6d32312973ca4db13c2edfe3e0c5990a7354
Sergio Garcia <sergio.garcia@example.net>
2018-09-19 17:18:43 +0000

    Update default recipe to use template

--- workstation/recipes/setup.rb
+++ workstation/recipes/setup.rb
@@ -2,11 +2,6 @@ package 'tree' do
   action :install
 end
 
-file '/etc/motd' do
-  content "Property of Sergio\nhi!
-
-  IPADDRESS: #{node['ipaddress']}
-  HOSTNAME:  #{node['hostname']}
-  MEMORY:    #{node['memory']['total']}
-  CPU:       #{node['cpu']['0']['mhz']}"
+template '/etc/motd' do
+  source 'motd.erb'
 end
