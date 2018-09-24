aa68685b4c95e0304e50a2571be6bc3bd1272726
Sergio Garcia <sergio@example.net>
2018-09-24 21:09:33 +0200

    Update production environment to apache~0.3.3

--- environments/production.rb
+++ environments/production.rb
@@ -1,7 +1,7 @@
 name 'production'
 description 'Where we run production code'
 
-cookbook 'apache', '= 0.3.2'
+cookbook 'apache', '= 0.3.3'
 override_attributes({
   "apache" => {
     "port" => 8181
