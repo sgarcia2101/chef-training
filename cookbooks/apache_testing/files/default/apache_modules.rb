9dca6dea7e226b4fa145b6a8f1de7204b9604f3e
Sergio Garcia <sergio@example.net>
2018-09-27 21:05:48 +0200

    Add version to ohai apache module

--- cookbooks/apache_testing/files/default/apache_modules.rb
+++ cookbooks/apache_testing/files/default/apache_modules.rb
@@ -5,5 +5,14 @@ Ohai.plugin :Apache do
     apache(Mash.new)
     modules_cmd = shell_out('apachectl -t -D DUMP_MODULES')
     apache[:modules] = modules_cmd.stdout
+
+    so = shell_out("apachectl -v")
+    # Sample output:
+    # Server version: Apache/2.2.15 (Unix)
+    # Server built:   Jun 19 2018 15:45:13
+    if so.exitstatus == 0
+      output = so.stdout.split
+      apache[:version] = output[2]
+    end
   end
 end
